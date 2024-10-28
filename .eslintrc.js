d1b0c648b323844c8b14dec7cc43c246cbba0277
Harvindsokhal <harvindsokhal@gmail.com>
2024-10-28 19:53:03 +0000

    Project cleanup & prettier/es-lint setup

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+const config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint/eslint-plugin'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
+
+export default config
