92addeb36fddf7470b3ff0cac33e166ca139ea67
Harvindsokhal <harvindsokhal@gmail.com>
2024-10-28 22:46:44 +0000

    adjust toggle for 3 options & scale down view media queries w/ vertical slider

--- src/components/AnswersToggle/AnswersToggle.tsx
+++ src/components/AnswersToggle/AnswersToggle.tsx
@@ -19,10 +19,18 @@ const AnswersToggle = ({
   isLocked,
 }: AnswersToggleProps) => {
   const [currentOptionId, setCurrentOptionId] = useState(options[0].id)
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
 
   useEffect(() => {
     setCurrentOptionId(selectedOptionId)
   }, [selectedOptionId])
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768)
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
   const handleToggle = (optionId: number) => {
     if (isLocked || optionId === selectedOptionId) return
@@ -35,14 +43,37 @@ const AnswersToggle = ({
     (option) => option.id === selectedOptionId
   )
 
+  const hasLongTextOption = options.some((option) => option.text.length > 20)
+
+  const sliderSize = 100 / options.length
+  const positionPercentage = selectedIndex * 100
+
   return (
-    <div className={'answers-toggle-container'}>
+    <div
+      className={`answers-toggle-container ${
+        hasLongTextOption && isMobile ? 'wrap' : ''
+      }`}
+    >
       <motion.div
-        className={'toggle-slider'}
+        className={`toggle-slider ${
+          hasLongTextOption && isMobile ? 'wrap' : ''
+        }`}
+        initial={{
+          y: isMobile && hasLongTextOption ? `${positionPercentage}%` : 0,
+          x: !isMobile || !hasLongTextOption ? `${positionPercentage}%` : 0,
+        }}
         animate={{
-          x: `${selectedIndex * 100}%`,
+          x: !isMobile || !hasLongTextOption ? `${positionPercentage}%` : 0,
+          y:
+            hasLongTextOption && isMobile
+              ? `${positionPercentage}%`
+              : undefined,
         }}
         transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+        style={{
+          width: !isMobile || !hasLongTextOption ? `${sliderSize}%` : '100%',
+          height: isMobile && hasLongTextOption ? `${sliderSize}%` : '100%',
+        }}
       />
       {options.map((option) => {
         return (
