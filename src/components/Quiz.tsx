acb63fcbe3e048c1f6fdef5b1a38187e9b91b291
Harvindsokhal <harvindsokhal@gmail.com>
2024-10-28 20:19:12 +0000

    react-query client/provider setup and use

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,6 +1,19 @@
+import { useQuery } from '@tanstack/react-query'
 import './Quiz.scss'
+import { QuestionData } from '../models/question'
+import { fetchQuestions } from '../services/questions'
 
 const Quiz = () => {
+  const { data, isLoading, isError, error } = useQuery<QuestionData>({
+    queryKey: ['questions'],
+    queryFn: fetchQuestions,
+  })
+
+  console.log(data)
+
+  if (isLoading) return <p>Loading...</p>
+  if (isError) return <p>Error: {(error as Error).message}</p>
+
   return <div className="quiz-container">Quiz</div>
 }
 
